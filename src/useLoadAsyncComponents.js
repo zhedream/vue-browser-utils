80e83a72c5e0462ee61568746653042f97b2ddb4
liuhaozhe <l19517863@163.com>
2023-03-27 14:36:49 +0800

    update

--- src/useLoadAsyncComponents.js
+++ src/useLoadAsyncComponents.js
@@ -1,51 +1,84 @@
-const useDeFineComponent = (Vue, template, resolve) => (call) => {
-  let component = call(Vue, template);
-  resolve(component);
-  return component;
-};
-
-function loadAsyncComponent(url, name) {
-  return () => {
-    let comResolve;
-    let com = new Promise((res) => (comResolve = res));
-
-    // 加载组件
-    fetch(url)
-      .then((res) => res.text())
-      .then((html) => {
-        let div = document.createElement("div");
-        div.innerHTML = html;
-        const template = div.querySelector("template").innerHTML;
-
-        // https://stackoverflow.com/questions/11513392/how-to-detect-when-innerhtml-is-complete
-        // https://stackoverflow.com/questions/1197575/can-scripts-be-inserted-with-innerhtml
-        const script = div.querySelector("script");
-        const style = div.querySelector("style");
-        const defineComponent = useDeFineComponent(Vue, template, (component) =>
-          comResolve(component)
-        );
-        const script_text = script.innerHTML.trim();
-        const script_text_eval = `${script_text}`;
-        eval(script_text_eval);
-
-        // 加载样式
-        if (style) {
-          // TODO: 可以使用文件hash值,作为 id
-          style.id = "style_" + name;
-          if (document.getElementById("style_" + name)) {
-            document.getElementById("style_" + name).innerHTML = style.innerHTML;
-          } else {
-            document.head.appendChild(style);
-          }
-        }
-
-        div.remove();
-        div = null;
-      });
-
-    return com;
+var loadAsyncComponent = ((Vue, less) => {
+  const useDeFineComponent = (Vue, template, resolve) => (call) => {
+    // 接收一个普通 vue 组件对象, 或 Promise 对象
+    let component = call(Vue, template);
+    resolve(component);
+    return component;
   };
-}
+
+  const comCache = {};
+  const loadFnCahce = {};
+  function loadAsyncComponent(url, isCache = true) {
+    let key = url.split("/").join("_");
+
+    // 保证返回同一个函数, 保证同一个组件只加载一次
+    if (isCache && loadFnCahce[key]) {
+      return loadFnCahce[key];
+    } else {
+      loadFnCahce[key] = () => {
+        let comResolve, comReject;
+        if (isCache && comCache[key]) {
+          return comCache[key];
+        } else {
+          comCache[key] = new Promise((res, rej) => {
+            comResolve = res;
+            comReject = rej;
+          });
+        }
+        // 加载组件
+        fetch(url)
+          .then((res) => res.text())
+          .then(async (html) => {
+            let div = document.createElement("div");
+            div.innerHTML = html;
+            const template = div.querySelector("template").innerHTML;
+
+            // https://stackoverflow.com/questions/11513392/how-to-detect-when-innerhtml-is-complete
+            // https://stackoverflow.com/questions/1197575/can-scripts-be-inserted-with-innerhtml
+            const script = div.querySelector("script");
+            const style = div.querySelector("style");
+            if (style && style.lang === "less") {
+              let r = await less.render(style.innerHTML);
+              style.innerHTML = r.css;
+            }
+            const defineComponent = useDeFineComponent(Vue, template, comResolve);
+            const script_text = script.innerHTML.trim();
+            const script_text_eval = `(async ()=>{ 
+              try{
+                ${script_text} 
+              }catch(e){
+                console.error('加载模块失败', url, e);
+                comReject();
+              }
+            })() `;
+
+            eval(script_text_eval);
+
+            // 加载样式
+            if (style) {
+              style.id = "style_" + key;
+              if (document.getElementById("style_" + key)) {
+                document.getElementById("style_" + key).innerHTML = style.innerHTML;
+              } else {
+                document.head.appendChild(style);
+              }
+            }
+
+            div.remove();
+            div = null;
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+
+        return comCache[key];
+      };
+      return loadFnCahce[key];
+    }
+  }
+
+  return loadAsyncComponent;
+})(Vue, less);
 
 module.exports = { loadAsyncComponent };
 
